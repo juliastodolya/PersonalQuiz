fad45156c0d809f43a60d654b3bedb30353eaf04
Julia Stodolya <y.stodolya@gmail.com>
2020-05-30 18:23:40 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Юлия on 30.05.2020.
+//  Copyright © 2020 Julia Stodolya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

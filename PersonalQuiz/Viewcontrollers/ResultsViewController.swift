c31f274f61d03751b0a71e29a11630d57a3f0977
Julia Stodolya <y.stodolya@gmail.com>
2020-06-03 21:42:29 +0300

    home work 3.06.2020

--- PersonalQuiz/Viewcontrollers/ResultsViewController.swift
+++ PersonalQuiz/Viewcontrollers/ResultsViewController.swift
@@ -10,9 +10,58 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
+    //MARK: - IB Outlets
+    
+    @IBOutlet var resultEmodjiLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    
+    //MARK: - Public properties
+    var answersChoosen: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        resultEmodjiLabel.text = "Вы - \(answersSort(for: answersChoosen).rawValue)!"
+        resultDefinitionLabel.text = "\(answersSort(for: answersChoosen).definition)"
+        navigationItem.hidesBackButton = true
     }
+    
+    
+    
+    // MARK: - Private methods
+    private func answersSort(for answers: [Answer]) -> AnimalType {
+        var dogCount = 0
+        var catCount = 0
+        var turtleCount = 0
+        var rabbitCount = 0
+        
+        for answer in answers {
+            switch answer.type {
+            case .dog:
+                dogCount += 1
+            case .cat:
+                catCount += 1
+            case .turtle:
+                turtleCount += 1
+            case .rabbit:
+                rabbitCount += 1
+            }
+        }
+        var numAnimals = [dogCount, catCount, turtleCount, rabbitCount]
+        numAnimals = numAnimals.sorted()
+        let numWinner = numAnimals.last
+        
+        if numWinner == dogCount {
+            return .dog
+        } else if numWinner == catCount {
+            return .cat
+        } else if numWinner == turtleCount {
+            return .turtle
+        } else {
+            return .rabbit
+        }
+    }
+        
 }
+
+
